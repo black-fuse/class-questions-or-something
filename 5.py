15a730b49b9f5e6cfc501d8cc97534d96246fb02
Tekad <alessandroconnell@gmail.com>
2024-11-14 17:21:06 +0000

    file rearangement

--- 5.py
+++
@@ -1,3 +0,0 @@
-from bcolors import bcolors
-
-print(bcolors.OKCYAN,"123","456","7890",bcolors.ENDC, sep="-")
---
+++ end, sep/5.py
@@ -0,0 +1,3 @@
+from bcolors import bcolors
+
+print(bcolors.BG_RED,"123","456","7890",bcolors.ENDC, sep="-")
